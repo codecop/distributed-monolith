c86d69b52051029a33bd7ba4c3be4aa14c2d5c7c
Peter Kofler <peter.kofler@code-cop.org>
2022-11-24 15:15:04 +0100

    Make JPA start.

---
+++ src/main/java/org/codecop/Student.java
@@ -0,0 +1,17 @@
+package org.codecop;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // fields, getters and setters
+
+}
