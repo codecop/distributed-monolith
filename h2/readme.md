41b04c46f51904fc961cb85f512b07bf3152ece5
Peter Kofler <peter.kofler@code-cop.org>
2022-11-25 15:29:01 +0100

    Standalone H2 for shared DB.

---
+++ h2/readme.md
@@ -0,0 +1,16 @@
+# Distributed Monolith Service 'h2'
+
+This is an [H2 Database Engine](http://h2database.com/).
+
+## Running it locally
+
+You can run it with the exec maven plugin like this:
+
+```
+cd h2
+../mvnw compile exec:java
+```
+
+Or you can run it from your IDE.
+
+You can then access web console UI here: http://localhost:8082/
---
+++ h2/src/main/java/org/codecop/monolith/h2/Start.java
@@ -0,0 +1,19 @@
+package org.codecop.monolith.h2;
+
+public class Start {
+
+    public static void main(String[] args) throws Exception {
+        // see https://www.h2database.com/javadoc/org/h2/tools/Server.html
+        org.h2.tools.Server.main(new String[] { //
+                "-tcp", //
+                "-tcpPort", "9092", //
+                "-tcpAllowOthers", //
+                "-tcpPassword", "secretShutdown99", //
+                "-ifNotExists", //
+                // debug web console
+                "-web", //
+                "-webPort", "8082", //
+        });
+    }
+
+}
