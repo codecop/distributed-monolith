382e766268e7a1843e136bbe946b35ae926822af
Peter Kofler <peter.kofler@code-cop.org>
2022-12-19 20:30:40 +0100

    Reuse converter.

--- cell/src/main/java/org/codecop/monolith/playground/client/AliveListener.java
+++ cell/src/main/java/org/codecop/monolith/playground/client/AliveListener.java
@@ -5,12 +5,14 @@ import static io.micronaut.jms.activemq.classic.configuration.ActiveMqClassicCon
 import java.util.ArrayList;
 import java.util.List;
 
-import org.codecop.monolith.playground.gol.Position;
 import org.codecop.monolith.playground.events.ClockedPosition;
+import org.codecop.monolith.playground.events.ClockedPositionConverter;
+import org.codecop.monolith.playground.gol.Position;
 
 import io.micronaut.jms.annotations.JMSListener;
 import io.micronaut.jms.annotations.Topic;
 import io.micronaut.messaging.annotation.MessageBody;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
@@ -19,13 +21,12 @@ public class AliveListener {
 
     final List<Position> livingCells = new ArrayList<>();
 
+    @Inject
+    private ClockedPositionConverter converter;
+
     @Topic(value = "${config.jms.aliveQueue}")
     public void onLivingNeighbour(@MessageBody ClockedPosition message) {
-        livingCells.add(fromDto(message));
-    }
-
-    private Position fromDto(ClockedPosition message) {
-        return new Position(message.getX(), message.getY());
+        livingCells.add(converter.fromDto(message));
     }
 
     @Topic(value = "${config.jms.tickQueue}")
