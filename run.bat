c6860988731d9e29b62011db61ec166d0e987c79
Peter Kofler <peter.kofler@code-cop.org>
2022-12-28 13:48:17 +0100

    Show state of each node during test run.

--- run.bat
+++ run.bat
@@ -1,47 +1,92 @@
 @setlocal
-@set BUILD=call mvn clean package -DskipTests
+@cls
+call mvn clean package -DskipTests
 
-@echo start activemq
+@echo ----------------------------------------
+@echo start ActiveMQ
 cd activemq
-%BUILD%
 @rem cls && call mvn clean compile exec:java
-start "ActiveMQ" cmd.exe /C "java -jar target\distributed-gol-activemq-0.1-jar-with-dependencies.jar"
+@set JAR=-jar target\distributed-gol-activemq-0.1-jar-with-dependencies.jar
+start "ActiveMQ" cmd.exe /C "java %JAR%"
 cd ..
+@echo wait for ActiveMQ to start
+@pause
 
+
+@echo ----------------------------------------
 @echo starting cells
 cd cell
-%BUILD%
 @rem cls && call mvn mn:run
-start "Cell00" cmd.exe /C "java -Dmicronaut.server.port=8000 -Dposition.x=0 -Dposition.y=0 -jar target\distributed-gol-cell-0.1.jar"
-start "Cell10" cmd.exe /C "java -Dmicronaut.server.port=8001 -Dposition.x=1 -Dposition.y=0 -jar target\distributed-gol-cell-0.1.jar"
-start "Cell20" cmd.exe /C "java -Dmicronaut.server.port=8002 -Dposition.x=2 -Dposition.y=0 -jar target\distributed-gol-cell-0.1.jar"
+@set JAR=-jar target\distributed-gol-cell-0.1.jar
+start "Cell00" cmd.exe /C "java -Dmicronaut.server.port=8000 -Dposition.x=0 -Dposition.y=0 %JAR%"
+start "Cell10" cmd.exe /C "java -Dmicronaut.server.port=8001 -Dposition.x=1 -Dposition.y=0 %JAR%"
+start "Cell20" cmd.exe /C "java -Dmicronaut.server.port=8002 -Dposition.x=2 -Dposition.y=0 %JAR%"
 
-start "Cell01" cmd.exe /C "java -Dmicronaut.server.port=8100 -Dposition.x=0 -Dposition.y=1 -jar target\distributed-gol-cell-0.1.jar"
-start "Cell11" cmd.exe /C "java -Dmicronaut.server.port=8101 -Dposition.x=1 -Dposition.y=1 -jar target\distributed-gol-cell-0.1.jar"
-start "Cell21" cmd.exe /C "java -Dmicronaut.server.port=8102 -Dposition.x=2 -Dposition.y=1 -jar target\distributed-gol-cell-0.1.jar"
+start "Cell01" cmd.exe /C "java -Dmicronaut.server.port=8100 -Dposition.x=0 -Dposition.y=1 %JAR%"
+start "Cell11" cmd.exe /C "java -Dmicronaut.server.port=8101 -Dposition.x=1 -Dposition.y=1 %JAR%"
+start "Cell21" cmd.exe /C "java -Dmicronaut.server.port=8102 -Dposition.x=2 -Dposition.y=1 %JAR%"
 
-start "Cell02" cmd.exe /C "java -Dmicronaut.server.port=8200 -Dposition.x=0 -Dposition.y=2 -jar target\distributed-gol-cell-0.1.jar"
-start "Cell12" cmd.exe /C "java -Dmicronaut.server.port=8201 -Dposition.x=1 -Dposition.y=2 -jar target\distributed-gol-cell-0.1.jar"
-start "Cell22" cmd.exe /C "java -Dmicronaut.server.port=8202 -Dposition.x=2 -Dposition.y=2 -jar target\distributed-gol-cell-0.1.jar"
+start "Cell02" cmd.exe /C "java -Dmicronaut.server.port=8200 -Dposition.x=0 -Dposition.y=2 %JAR%"
+start "Cell12" cmd.exe /C "java -Dmicronaut.server.port=8201 -Dposition.x=1 -Dposition.y=2 %JAR%"
+start "Cell22" cmd.exe /C "java -Dmicronaut.server.port=8202 -Dposition.x=2 -Dposition.y=2 %JAR%"
 cd ..
+@echo wait for servers to start
+@pause
 
-call wget --method=post http://localhost:8100/seed && del seed
-call wget --method=post http://localhost:8101/seed && del seed
-call wget --method=post http://localhost:8102/seed && del seed
+
+@set OPTS=-q -O-
+@echo ----------------------------------------
+@echo seeding
+wget %OPTS% --method=post http://localhost:8100/seed
+wget %OPTS% --method=post http://localhost:8101/seed
+wget %OPTS% --method=post http://localhost:8102/seed
 @echo ready to view SEED?
 @pause
-wget http://localhost:8000/grid && type grid && del grid
+wget %OPTS% http://localhost:8000/grid
+wget %OPTS% http://localhost:8000/alive.json
+wget %OPTS% http://localhost:8001/alive.json
+wget %OPTS% http://localhost:8002/alive.json
+wget %OPTS% http://localhost:8100/alive.json
+wget %OPTS% http://localhost:8101/alive.json
+wget %OPTS% http://localhost:8102/alive.json
+wget %OPTS% http://localhost:8200/alive.json
+wget %OPTS% http://localhost:8201/alive.json
+wget %OPTS% http://localhost:8202/alive.json
+@echo ;
 
-
-call wget --method=post http://localhost:8102/tick && del tick 
+@echo ready to TICK?
+@pause
+wget %OPTS% --method=post http://localhost:8102/tick 
 @echo ready to view first TICK?
 @pause
-wget http://localhost:8000/grid && type grid && del grid
+wget %OPTS% http://localhost:8000/grid
+wget %OPTS% http://localhost:8000/alive.json
+wget %OPTS% http://localhost:8001/alive.json
+wget %OPTS% http://localhost:8002/alive.json
+wget %OPTS% http://localhost:8100/alive.json
+wget %OPTS% http://localhost:8101/alive.json
+wget %OPTS% http://localhost:8102/alive.json
+wget %OPTS% http://localhost:8200/alive.json
+wget %OPTS% http://localhost:8201/alive.json
+wget %OPTS% http://localhost:8202/alive.json
+@echo ;
 
 
-call wget --method=post http://localhost:8102/tick && del tick 
+@echo ready to TICK?
+@pause
+wget %OPTS% --method=post http://localhost:8102/tick 
 @echo ready to view next TICK?
 @pause
-wget http://localhost:8000/grid && type grid && del grid
+wget %OPTS% http://localhost:8000/grid
+wget %OPTS% http://localhost:8000/alive.json
+wget %OPTS% http://localhost:8001/alive.json
+wget %OPTS% http://localhost:8002/alive.json
+wget %OPTS% http://localhost:8100/alive.json
+wget %OPTS% http://localhost:8101/alive.json
+wget %OPTS% http://localhost:8102/alive.json
+wget %OPTS% http://localhost:8200/alive.json
+wget %OPTS% http://localhost:8201/alive.json
+wget %OPTS% http://localhost:8202/alive.json
+@echo ;
 
 @endlocal
