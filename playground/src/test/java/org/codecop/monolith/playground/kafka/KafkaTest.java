e2f4b7467835408fe51b4825f33ebc52cffffc06
Peter Kofler <peter.kofler@code-cop.org>
2023-02-26 21:45:09 +0100

    Use wait instead of sleep in test.

--- playground/src/test/java/org/codecop/monolith/playground/kafka/KafkaTest.java
+++ playground/src/test/java/org/codecop/monolith/playground/kafka/KafkaTest.java
@@ -1,6 +1,9 @@
 package org.codecop.monolith.playground.kafka;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.TimeUnit;
 
 import org.codecop.monolith.playground.jms.TextStore;
 import org.junit.jupiter.api.Test;
@@ -16,9 +19,9 @@ class KafkaTest {
 
     @Inject
     TextStore store;
-    
+
     @Test
-    void sendAndReceiveMessage() throws InterruptedException {
+    void sendAndReceiveMessage() {
         assertEquals(0, store.messages.size());
 
         BookAnalytics book = new BookAnalytics("1234", 1);
@@ -28,7 +31,7 @@ class KafkaTest {
         assertEquals(1, store.messages.size());
     }
 
-    private void waitForKafka() throws InterruptedException {
-        Thread.sleep(5_000);
+    private void waitForKafka() {
+        await().atMost(5, TimeUnit.SECONDS).until(() -> !store.messages.isEmpty());
     }
 }
